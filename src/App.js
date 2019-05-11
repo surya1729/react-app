fcd1d95e84a0700d1f47a06733990de07857c1d8
surya <suryapratap24999@gmail.com>
2019-05-11 14:57:59 +0530

    passing method reference b/w components and bind syntax

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React,{ useState } from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Person from './Person/Person';// file extension doesn't need to have any name 
 
-const app = props => {
-    const [ personsState, setPersonsState] = useState(
-      {
-      persons: [
-        {name: 'surya', age: 18},
-        {name: 'pratap', age: 8},
-        {name: 'my', age: 25}
-      ]
-    });
-    const [otherState,setOtherState] = useState("some other value");
-    console.log(personsState,otherState);
-    const switchNameHandler = () => {
-      //console.log('was clicked');
-      // DON'T DO THIS: this.state.persons[0].name = 'suryapratap'
-      setPersonsState(  {
-        persons: [
-           {name: 'suryachalumuri', age: 18},
-           {name: 'pratap', age: 8},
-           {name: 'my', age: 20}
-        ]
-      } ); 
-     };
+class App extends Component {
+   state = {
+     persons: [
+       {name: 'surya', age: 18},
+       {name: 'pratap', age: 8},
+       {name: 'my', age: 25}
+     ],
+     otherState: 'some other state;'
+   };
 
+   switchNameHandler = (newName) => {
+     //console.log('was clicked');
+     // DON'T DO THIS: this.state.persons[0].name = 'suryapratap'
+     this.setState(  {
+       persons: [
+          {name: newName, age: 18},
+          {name: 'pratap', age: 8},
+          {name: 'my', age: 20}
+       ]
+     } ); 
+    };
+  render(){
   return (
     <div className="App">
       <h1>Hi,I'm a React App </h1>
       <p>this is really working!</p> 
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name = {personsState.persons[0].name} age = {personsState.persons[0].age}/>
-      <Person name = {personsState.persons[1].name} age = {personsState.persons[1].age}>My Hobbies: Reading</Person>
-      <Person name = {personsState.persons[2].name} age = {personsState.persons[2].age}/>
+      <button onClick={() => this.switchNameHandler('chalumuri!!')}>Switch Name</button>
+      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}
+      click={this.switchNameHandler.bind(this, 'surya!!')}>My Hobbies: Reading</Person>
+      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
     </div>
 
   );
@@ -40,7 +40,17 @@ const app = props => {
    // to generate button in host
         /*make sure that switchNameHandler() is not called as we just calling its reference so it is switchNameHandler*/
    
+  }
 }
-export default app;
-/*this should be removed because we are not using 
-any class we just using only function */
+export default App;
+/*<button onClick={this.switchNameHandler.bind(this, 'chalumuri')}>Switch Name</button> 
+while clicking switch name button then surya is replaced with chalumuri
+and similarly 
+ <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}
+      click={this.switchNameHandler.bind(this, 'surya!!')}>My Hobbies: Reading</Person>
+by clicking this link in output file it surya/chalumuri changes to surya!!*/
+
+
+/*switchNameHandler() by using this we can pass arguments but it
+is somewhat inefficent ...
+it is better to "bind" then the above */
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const person = (props) => {  //argument name be anything
     return(  
     
      <div>    
-    <p>I'm {props.name} and I am {props.age} years old!</p>
+    <p onClick={props.click}> I'm {props.name} and I am {props.age} years old!</p>
     <p>{props.children}</p>
     
       </div>
