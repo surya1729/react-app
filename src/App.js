15d73d37846e05d3871b6eb95ad70e853401cdd4
surya <suryapratap24999@gmail.com>
2019-05-11 12:10:00 +0530

    with state and setstate in class App which it extends from component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Person from './Person/Person';// file extension doesn't need to have any name 
 
-function App() {
+class App extends Component {
+   state = {
+     persons: [
+       {name: 'surya', age: 18},
+       {name: 'pratap', age: 8},
+       {name: 'my', age: 25}
+     ],
+     otherState: 'some other state;'
+   };
+
+   switchNameHandler = () => {
+     //console.log('was clicked');
+     // DON'T DO THIS: this.state.persons[0].name = 'suryapratap'
+     this.setState(  {
+       persons: [
+          {name: 'suryachalumuri', age: 18},
+          {name: 'pratap', age: 8},
+          {name: 'my', age: 20}
+       ]
+     } ); 
+    };
+  render(){
   return (
     <div className="App">
       <h1>Hi,I'm a React App </h1>
-      <p>this is really working!</p>  
+      <p>this is really working!</p> 
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My Hobbies: Reading</Person>
+      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
     </div>
 
   );
   //return React.createElement('div',{className: 'App'},React.createElement('h1',null, 'does this work now?'));
+   // to generate button in host
+        /*make sure that switchNameHandler() is not called as we just calling its reference so it is switchNameHandler*/
+   
+  }
 }
-
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const person = (props) => {  //argument name be anything
+    
+    return(  
+    
+     <div>    
+    <p>I'm {props.name} and I am {props.age} years old!</p>
+    <p>{props.children}</p>
+    
+      </div>
+    )
+};
+
+export default person;
+
+
+/* class Person extends Component{
+    render(){
+        return <p>My name is {this.props}</p>
+    }
+    while using class based components it's this.props
+}*/ 
